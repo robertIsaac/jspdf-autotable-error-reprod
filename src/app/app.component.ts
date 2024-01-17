c0c1e960e5b9d11918534d9ec8d4bc6e7c8df484
robert-isaac_emplc <robert.isaac@fastmarkets.com>
2024-01-17 17:37:54 +0200

    reproduce the problem
    shows function in serve (which is what's expected)
    shows object in build (which is not correct

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,6 +9,13 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'jspdf-autotable-error-reprod';
+export class AppComponent implements OnInit {
+  private autoTable: unknown;
+  autoTableType?: string;
+
+  async ngOnInit() {
+    this.autoTable = await import('jspdf-autotable').then((library) => library.default);
+    console.log(this.autoTable);
+    this.autoTableType = typeof this.autoTable;
+  }
 }
